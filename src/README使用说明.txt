8ec04ab29caff4c979e1bd29f9755789b453fc06
Command <maoxs2@163.com>
2018-06-17 21:57:28 +0800

    add onekey_start & chinese readme

---
+++ src/README使用说明.txt
@@ -0,0 +1,5 @@
+快速开始：使用双击onekey_start.bat文件，输入账号立即开始。
+
+高级：修改config.json文件后直接双击webchain-miner.exe启动。
+
+WebChain官方QQ群694046912
--- src/Summary.cpp
+++ src/Summary.cpp
@@ -103,20 +103,17 @@ static void print_threads(xmrig::Config *config)
             buf[0] = '\0';
         }
 
-        Log::i()->text(config->isColors() ? "\x1B[01;32m * \x1B[01;37mTHREADS:      \x1B[01;36m%d\x1B[01;37m, %s, av=%d, %sdonate=%d%%%s" : " * THREADS:      %d, %s, av=%d, %sdonate=%d%%%s",
+        Log::i()->text(config->isColors() ? "\x1B[01;32m * \x1B[01;37mTHREADS:      \x1B[01;36m%d\x1B[01;37m, %s, av=%d " : " * THREADS:      %d, %s, av=%d ",
                        config->threadsCount(),
                        config->algorithm().name(),
                        config->algoVariant(),
-                       config->isColors() && config->donateLevel() == 0 ? "\x1B[01;31m" : "",
-                       config->donateLevel(),
                        buf);
     }
     else {
-        Log::i()->text(config->isColors() ? "\x1B[01;32m * \x1B[01;37mTHREADS:      \x1B[01;36m%d\x1B[01;37m, %s, %sdonate=%d%%" : " * THREADS:      %d, %s, %sdonate=%d%%",
+        Log::i()->text(config->isColors() ? "\x1B[01;32m * \x1B[01;37mTHREADS:      \x1B[01;36m%d\x1B[01;37m, %s " : " * THREADS:      %d, %s ",
                        config->threadsCount(),
                        config->algorithm().name(),
-                       config->isColors() && config->donateLevel() == 0 ? "\x1B[01;31m" : "",
-                       config->donateLevel());
+                       );
     }
 }
 
